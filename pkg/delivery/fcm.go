cc47d7e2c3e65936f34bf562869fc8155121bd82
Marco Mele <marco@hopscotch.trade>
2025-08-08 16:11:00 -0500

    XmtpNotificationData

--- pkg/delivery/fcm.go
+++ pkg/delivery/fcm.go
@@ -61,13 +61,12 @@ func (f FcmDelivery) Send(ctx context.Context, req interfaces.SendRequest) error
 	message := base64.StdEncoding.EncodeToString(req.Message.Message)
 	topic := req.Message.ContentTopic
 	data := map[string]string{
-		"topic": topic,
+		"channel":         "chat",
+		"deduplicationId": req.IdempotencyKey,
 		// "encryptedMessage": message,
-		"messageType":    string(req.MessageContext.MessageType),
 		"installationId": req.Installation.Id,
-		"channel":        "chat",
-		"timestampNs":    fmt.Sprint(req.Message.TimestampNs),
-		"tag":            req.IdempotencyKey,
+		"messageType":    string(req.MessageContext.MessageType),
+		"topic":          topic,
 	}
 
 	prefix := ""
